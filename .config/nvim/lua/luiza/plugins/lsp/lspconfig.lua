3027d20429dba36587ef531df56389c9cd64662c
Luiza Carvalho <luizacarvalho@Luizas-MacBook-Pro.local>
2026-03-05 20:58:21 +0100

    blink works?

--- .config/nvim/lua/luiza/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/luiza/plugins/lsp/lspconfig.lua
@@ -5,13 +5,13 @@ return {
 	dependencies = {
 		"mason-org/mason.nvim",
 		"mason-org/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
+		-- "hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", enabled = false, opts = {} },
 	},
 	config = function()
 		local mason_lspconfig = require("mason-lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local map = vim.keymap.set
 
@@ -83,7 +83,8 @@ return {
 			end,
 		})
 
-		local capabilities = cmp_nvim_lsp.default_capabilities() -- to enable autocompletion (assign to every lsp server config)
+		-- local capabilities = cmp_nvim_lsp.default_capabilities() -- to enable autocompletion (assign to every lsp server config)
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
