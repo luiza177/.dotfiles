abbfd4988015de535ec6aa6cd3105d6e56877c55
Luiza Carvalho <luizacarvalho@Luizas-MacBook-Pro.local>
2026-03-08 13:44:52 +0100

    theme change

---
+++ .config/nvim/lua/luiza/plugins/themes/kanagawa.lua
@@ -0,0 +1,16 @@
+return {
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	opts = {
+		dimInactive = true,
+		colors = {
+			theme = {
+				all = {
+					ui = {
+						bg_gutter = "none",
+					},
+				},
+			},
+		},
+	},
+}
