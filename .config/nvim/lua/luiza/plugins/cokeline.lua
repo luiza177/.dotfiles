4ff1d4505553004bdd2eb568edc77c6446a3147f
Luiza Carvalho <luiza177music@gmail.com>
2026-03-15 21:58:45 +0100

    fully configured

--- .config/nvim/lua/luiza/plugins/cokeline.lua
+++ .config/nvim/lua/luiza/plugins/cokeline.lua
@@ -5,9 +5,97 @@ return {
 		"nvim-tree/nvim-web-devicons", -- If you want devicons
 		-- "stevearc/resession.nvim", -- Optional, for persistent history
 	},
-	opts = {
-		sidebar = {
-			filetype = "neo-tree",
-		},
-	},
+	config = function()
+		local get_hex = require("cokeline.hlgroups").get_hl_attr
+
+		require("cokeline").setup({
+			default_hl = {
+				fg = function(buffer)
+					return buffer.is_focused and get_hex("Normal", "fg") or get_hex("LineNr", "fg")
+				end,
+				bg = function(buffer)
+					return buffer.is_focused and get_hex("Normal", "bg") or get_hex("WinSeparator", "bg") -- TODO: until the end
+					-- return buffer.is_focused and get_hex("CursorLine", "bg") or get_hex("Normal", "bg")
+				end,
+				-- underline = true,
+				-- sp = get_hex("LineNr", "fg"),
+			},
+			fill_hl = "WinSeparator",
+			sidebar = {
+				filetype = { " neo-tree" },
+				components = {
+					{
+						text = "              Neo-tree",
+						fg = function()
+							return get_hex("NeoTreeNormal", "fg")
+						end,
+						bg = function()
+							return get_hex("NeoTreeNormalNC", "bg")
+						end,
+						bold = function(buffer)
+							return buffer.is_focused
+						end,
+					},
+				},
+			},
+			components = {
+				-- {
+				-- 	text = function(buffer)
+				-- 		return (buffer.index ~= 1) and "│" or ""
+				-- 	end,
+				-- 	-- text = "│",
+				-- 	fg = function()
+				-- 		return get_hex("LineNr", "fg")
+				-- 		-- return buffer.is_focused and get_hex("String", "fg") or get_hex("LineNr", "fg")
+				-- 	end,
+				-- },
+				{
+					text = function(buffer)
+						return "  " .. buffer.devicon.icon
+					end,
+					fg = function(buffer)
+						return buffer.devicon.color
+					end,
+				},
+				{
+					text = function(buffer)
+						return buffer.filename .. "  "
+					end,
+					bold = function(buffer)
+						return buffer.is_focused
+					end,
+					italic = function(buffer)
+						return buffer.is_modified
+					end,
+				},
+				{
+					-- text = "󰖭",
+					text = function(buffer)
+						return buffer.is_modified and "" or "󰖭"
+					end,
+					fg = function(buffer)
+						if buffer.is_focused then
+							if buffer.is_modified then
+								return get_hex("String", "fg")
+							else
+								return get_hex("Normal", "fg")
+							end
+						else
+							return get_hex("LineNr", "fg")
+						end
+					end,
+					on_click = function(_, _, _, _, buffer)
+						buffer:delete()
+					end,
+				},
+				{
+					text = "  │",
+					fg = function()
+						return get_hex("LineNr", "fg")
+						-- return buffer.is_focused and get_hex("String", "fg") or get_hex("LineNr", "fg")
+					end,
+				},
+			},
+		})
+	end,
 }
--- .config/nvim/lua/luiza/plugins/virt-column.lua
+++ .config/nvim/lua/luiza/plugins/virt-column.lua
@@ -1,9 +1,9 @@
 return {
-  "lukas-reineke/virt-column.nvim",
-  event = "BufEnter",
-  opts = {
-    char = "│", -- the line characte, not | pipe
-    highlight = "VirtColumn",
-    virtcolumn = "100",
-  },
+	"lukas-reineke/virt-column.nvim",
+	event = "BufEnter",
+	opts = {
+		char = "│", -- the line character, not | pipe
+		highlight = "VirtColumn",
+		virtcolumn = "100",
+	},
 }
