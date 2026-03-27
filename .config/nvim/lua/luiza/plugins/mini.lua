34e6b87904ae5e94b2bf5f2000ffcb8bd0041a2f
Luiza Carvalho <luiza177music@gmail.com>
2026-03-27 19:23:26 -0300

    mini, remove grug

--- .config/nvim/lua/luiza/plugins/mini.lua
+++ .config/nvim/lua/luiza/plugins/mini.lua
@@ -1,5 +1,5 @@
 return {
-	{ "nvim-mini/mini.ai", version = false, enabled = false },
 	-- { 'nvim-mini/mini.surround', version = false },
-	-- { 'nvim-mini/mini.map', version = false },
+	{ "nvim-mini/mini.map", version = false, config = true },
+	{ "nvim-mini/mini.sessions", version = false, enabled = true, config = true },
 }
