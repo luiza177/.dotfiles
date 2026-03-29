ec04e6a10e34499ab4a9495785008beca6d68891
Luiza Carvalho <luiza177music@gmail.com>
2026-03-29 10:07:36 -0300

    todos mini etc

--- .config/nvim/lua/luiza/plugins/mini.lua
+++ .config/nvim/lua/luiza/plugins/mini.lua
@@ -1,5 +1,8 @@
 return {
-	-- { 'nvim-mini/mini.surround', version = false },
+	-- { 'nvim-mini/mini.surround', version = false }, -- TODO: replace surround?
 	{ "nvim-mini/mini.map", version = false, config = true },
 	{ "nvim-mini/mini.sessions", version = false, enabled = true, config = true },
+	-- TODO: replace neo-tree with mini.files?
+	-- Q: replace autopairs?
+	-- Q: mini.bracketed? for nav
 }
