3f874189fdd68bfa32c87ae31603d7a3c360b5b2
Luiza Carvalho <luiza177music@gmail.com>
2026-04-19 09:18:01 +0200

    mini keymaps in surround

--- .config/nvim/lua/luiza/plugins/surround.lua
+++ .config/nvim/lua/luiza/plugins/surround.lua
@@ -1,5 +1,15 @@
 return {
-  "kylechui/nvim-surround",
-  event = "VeryLazy",
-  config = true,
+	"kylechui/nvim-surround",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup()
+
+		vim.g.nvim_surround_no_normal_mappings = true
+
+		vim.keymap.set("n", "sa", "<Plug>(nvim-surround-normal)", { desc = "Surround add" })
+		vim.keymap.set("n", "sA", "<Plug>(nvim-surround-normal-line)", { desc = "Surround add (line)" })
+		vim.keymap.set("n", "sd", "<Plug>(nvim-surround-delete)", { desc = "Surround delete" })
+		vim.keymap.set("n", "sr", "<Plug>(nvim-surround-change)", { desc = "Surround change" })
+		vim.keymap.set("x", "sa", "<Plug>(nvim-surround-visual)", { desc = "Surround add (visual)" })
+	end,
 }
